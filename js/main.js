ef6613a65bb0007a5ef3c757beb7447351b9230b
njang <neo.w.jang@gmail.com>
2017-12-19 11:33:14 -0600

    Revise style and layout

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+// const buildNav = () => {
+
+
+// 	// let formElement = document.createElement('form');
+// 	// formElement.method = 'get';
+// 	// let inputElement = document.createElement('input');
+// 	// inputElement.id = 'inputText';
+// 	// inputElement.type = 'text';
+// 	// inputElement.placeholder = 'How was your day?';
+// 	// formElement.appendChild(inputElement);
+
+// 	let navbar = document.createElement('div');
+// 	navbar.class = "row";
+	
+// 	let buttonHome = document.createElement('button');
+// 	let buttonWorks = document.createElement('button');
+// 	let buttonContact = document.createElement('button');
+	
+// 	// navbar.appendChild(buttonHome)
+// 	// navbar.appendChild(buttonWorks)
+// 	// navbar.appendChild(buttonContact);
+
+// 	$('nav').appendChild(navbar);
+// 	// document.getElementById('input').appendChild(formElement);	
+// 	// document.getElementById('postButton').appendChild(buttonElement);
+// }
+
+// buildNav();
