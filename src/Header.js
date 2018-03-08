b68fdcd58260aa15579ca3d194e3acc59fb36d5e
njang <neo.w.jang@gmail.com>
2018-03-07 23:22:33 -0600

    Revise header

--- src/Header.js
+++ src/Header.js
@@ -1,23 +1,56 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import './Header.css';
 
 class Header extends Component {
+	constructor(props) {
+		super(props);
+		this.toggle = this.toggle.bind(this);
+		this.state = {
+		isOpen: false
+		};
+	}
+	toggle() {
+		this.setState({
+		isOpen: !this.state.isOpen
+		});
+	}
 	render() {
 		return (
-			<Container>
-				<Row>
-					<Col>
-						Header goes here
-					</Col>
-				</Row>	
-				<Row>
-                    <Col>.col</Col>
-                    <Col>.col</Col>
-                    <Col>.col</Col>
-                    <Col>.col</Col>
-                </Row>
-			</Container>
-		)
+			<div>
+				<Navbar expand='md'>
+					<NavbarBrand href='/'>Neo W. Jang</NavbarBrand>
+					<NavbarToggler onClick={this.toggle} />
+					<Collapse isOpen={this.state.isOpen} navbar>
+						<Nav className='ml-auto' navbar>
+							<NavItem>
+								<NavLink href='/components/'>Components</NavLink>
+							</NavItem>
+							<NavItem>
+								<NavLink href='https://github.com/reactstrap/reactstrap'>Github</NavLink>
+							</NavItem>
+							<UncontrolledDropdown nav inNavbar>
+								<DropdownToggle nav caret>
+									Options
+								</DropdownToggle>
+								<DropdownMenu >
+									<DropdownItem>
+										Option 1
+									</DropdownItem>
+									<DropdownItem>
+										Option 2
+									</DropdownItem>
+								<DropdownItem divider />
+									<DropdownItem>
+										Reset
+									</DropdownItem>
+								</DropdownMenu>
+							</UncontrolledDropdown>
+						</Nav>
+					</Collapse>
+				</Navbar>
+			</div>
+		);
 	}
 }
 
