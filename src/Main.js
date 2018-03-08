ffd64f4da3d0050e23cca4a83be4a891fb3f264a
njang <neo.w.jang@gmail.com>
2018-03-08 09:10:45 -0600

    Enable SPA

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
-// import './App.css';
-import Header from './Header';
-import 'bootstrap/dist/css/bootstrap.css';
+import { Route, NavLink, HashRouter } from "react-router-dom";
+// import 'bootstrap/dist/css/bootstrap.css';
+
+import Home from "./Home";
+import Stuff from "./Stuff";
+import Contact from "./Contact";
+
 
 class Main extends Component {
     render() {
         return (
-            <div className="App">
-                <Header />
-                Under QA testing...
-            </div>
+        	<HashRouter>
+	            <div className="App">
+					<h1>Simple SPA</h1>
+					<ul className="header">
+						<Route path="/" component={Home}/>
+			            <Route path="/stuff" component={Stuff}/>
+			            <Route path="/contact" component={Contact}/>
+					</ul>
+	            </div>
+        	</HashRouter>
         );
     }
 }
