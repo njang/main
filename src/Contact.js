8e101377bc4f1a2a819d917f485c2f7c7261d713
njang <neo.w.jang@gmail.com>
2018-03-08 16:18:10 -0600

    Link social contacts

--- src/Contact.js
+++ src/Contact.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
  
 class Contact extends Component {
-  render() {
-    return (
-      <div>
-        <h2>GOT QUESTIONS?</h2>
-        <p>The easiest thing to do is post on
-        our <a href="http://forum.kirupa.com">forums</a>.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <a className='row my-2' href='https://www.github.com/njang' target='_blank' rel='noopener noreferrer'>
+                    <img className='col' src='images/icons/github.svg' height='48px' alt='Github' />
+                </a>
+                <a className='row my-2' href='https://www.linkedin.com/in/neojang' target='_blank' rel='noopener noreferrer'>
+                    <img className='col' src='images/icons/linkedin.svg' height='48px' alt='LinkedIn' />
+                </a>
+                <a className='row my-2' href='mail:neo.w.jang@gmail.com'>
+                    <img className='col' src='images/icons/gmail.svg' height='48px' alt='Gmail' />
+                </a>
+            </div>
+        );
+    }
 }
  
 export default Contact;
