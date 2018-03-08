122b1d9bf9898bbb95cfc7d8b0117e3b09960eeb
njang <neo.w.jang@gmail.com>
2018-03-08 16:24:42 -0600

    Revise works component for showcasing projects

---
+++ src/Works.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+ 
+class Works extends Component {
+  render() {
+    return (
+		<div>
+			<h2>Works</h2>
+			<div>Daily Report - Becky Gates Children's Center</div>
+			<div>Neighborful - Neighborhood Produce Exchange</div>
+			<div>Aggieland Auto Concierge</div>
+			<div>Lawn Tracker</div>
+		</div>
+    );
+  }
+}
+ 
+export default Works;
