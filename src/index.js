f1661ca4b6f58fd8580b5ce8473d23ae163015bd
njang <neo.w.jang@gmail.com>
2018-03-09 12:58:56 -0600

    Revise styling for index

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import Main from './Main';
 import registerServiceWorker from './registerServiceWorker';
 
+console.log('Hi there, fellow developer! Thanks for checking under the hood.')
+
 ReactDOM.render(<Main />, document.getElementById('root'));
 registerServiceWorker();
