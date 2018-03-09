47be81b44358d33d43eef6bc3a7ad85ed1282eef
njang <neo.w.jang@gmail.com>
2018-03-09 12:58:36 -0600

    Display portfolio projects

--- src/Works.js
+++ src/Works.js
@@ -4,7 +4,6 @@ class Works extends Component {
   render() {
     return (
 		<div>
-			<h2>Works</h2>
 			<div>Daily Report - Becky Gates Children's Center</div>
 			<div>Neighborful - Neighborhood Produce Exchange</div>
 			<div>Aggieland Auto Concierge</div>
