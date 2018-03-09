5e1e3372c3f0d09d665e2c101a795a0619fea26b
njang <neo.w.jang@gmail.com>
2018-03-09 17:00:18 -0600

    Revise content alignment

--- src/Works.js
+++ src/Works.js
@@ -3,12 +3,20 @@ import React, { Component } from "react";
 class Works extends Component {
   render() {
     return (
-		<div>
-			<div>Daily Report - Becky Gates Children's Center</div>
-			<div>Neighborful - Neighborhood Produce Exchange</div>
-			<div>Aggieland Auto Concierge</div>
-			<div>Lawn Tracker</div>
-		</div>
+    	<div>
+			<div className='row justify-content-center'>
+				<div className='col-10 col-sm-6 col-md-4 col-lg-3'>Daily Report | Becky Gates Children's Center</div>
+			</div>
+			<div className='row justify-content-center'>
+				<div className='col-10 col-sm-6 col-md-4 col-lg-3'>Neighborful | Neighborhood Produce Exchange</div>
+			</div>
+			<div className='row justify-content-center'>
+				<div className='col-10 col-sm-6 col-md-4 col-lg-3'>Aggieland Auto Concierge</div>
+			</div>
+			<div className='row justify-content-center'>
+				<div className='col-10 col-sm-6 col-md-4 col-lg-3'>Lawn Tracker</div>
+			</div>
+    	</div>
     );
   }
 }
