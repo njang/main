d64eaad71e6e2fa2735f1c8619f4075742ebc5d9
njang <neo.w.jang@gmail.com>
2018-03-10 09:42:47 -0600

    Replace images with icons

--- src/Contact.js
+++ src/Contact.js
@@ -5,14 +5,18 @@ class Contact extends Component {
         return (
             <div>
                 <a className='row my-2' href='https://www.github.com/njang' target='_blank' rel='noopener noreferrer'>
-                    <img className='col' src='images/icons/github.svg' height='48px' alt='Github' />
+                    {/*<img className='col' src='images/icons/github.svg' height='48px' alt='Github' />*/}
+                    <i class="fab fa-github h1"></i>
                 </a>
                 <a className='row my-2' href='https://www.linkedin.com/in/neojang' target='_blank' rel='noopener noreferrer'>
-                    <img className='col' src='images/icons/linkedin.svg' height='48px' alt='LinkedIn' />
+                    {/*<img className='col' src='images/icons/linkedin.svg' height='48px' alt='LinkedIn' />*/}
+                    <i class="fab fa-linkedin h1"></i>
                 </a>
                 <a className='row my-2' href='mail:neo.w.jang@gmail.com'>
-                    <img className='col' src='images/icons/gmail.svg' height='48px' alt='Gmail' />
+                    {/*<img className='col' src='images/icons/gmail.svg' height='48px' alt='Gmail' />*/}
+                    <i class="fas fa-at h1"></i>
                 </a>
+                
             </div>
         );
     }
