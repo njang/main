48c4dd478051c4f333314df74f7e8258346ba19d
njang <neo.w.jang@gmail.com>
2018-03-12 11:24:05 -0500

    Refactor Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Routes from './config/routes';
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
