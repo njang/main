2b552c24a7b35736aeef2c3a02de0c644605ba65
njang <neo.w.jang@gmail.com>
2018-03-12 23:42:39 -0500

    Add contact form using formspree

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,19 +4,26 @@ class Contact extends Component {
     render() {
         return (
             <div>
-                <a className='row my-2' href='https://www.github.com/njang' target='_blank' rel='noopener noreferrer'>
+            <div className='row justify-content-center'>
+                <a href='https://www.github.com/njang' target='_blank' rel='noopener noreferrer'>
                     {/*<img className='col' src='images/icons/github.svg' height='48px' alt='Github' />*/}
-                    <i className='fab fa-github h1'></i>
+                    <i className='col fab fa-github h1'></i>
                 </a>
-                <a className='row my-2' href='https://www.linkedin.com/in/neojang' target='_blank' rel='noopener noreferrer'>
+                <a href='https://www.linkedin.com/in/neojang' target='_blank' rel='noopener noreferrer'>
                     {/*<img className='col' src='images/icons/linkedin.svg' height='48px' alt='LinkedIn' />*/}
-                    <i className='fab fa-linkedin h1'></i>
+                    <i className='col fab fa-linkedin h1'></i>
                 </a>
-                <a className='row my-2' href='mail:neo.w.jang@gmail.com'>
+                <a href='mail:neo.w.jang@gmail.com'>
                     {/*<img className='col' src='images/icons/gmail.svg' height='48px' alt='Gmail' />*/}
-                    <i className='fas fa-envelope h1'></i>
+                    <i className='col fas fa-envelope h1'></i>
                 </a>
                 
+            </div>
+            <form action="https://formspree.io/neo.w.jang@gmail.com" method="POST" className='form-group'>
+                <input type="text" name="name" className='form-control' />
+                <input type="email" name="_replyto" className='form-control' />
+                <input type="submit" value="Send" className='btn btn-success' />
+            </form>
             </div>
         );
     }
