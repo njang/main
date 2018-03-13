c30d89b43076ff7dd7ab52570859d6b8ed147879
njang <neo.w.jang@gmail.com>
2018-03-12 23:42:18 -0500

    Revise footer text & styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ class Footer extends Component {
 		return (
 			<footer className='py-5 bg-dark'>
 				<div className='container'>
-					<p className='m-0 text-center text-white'>Handcobbled with &#10084;</p>
-					<p className='m-0 text-center text-white'>Copyright &copy; Neo W. Jang 2018</p>
+					<p className='m-0 text-center text-white'>Handcobbled with <span className='text-danger'>&#10084;</span></p>
+					<p className='m-0 text-center text-white'>&copy; Neo W. Jang 2017-2018</p>
 				</div>
 			</footer>
 		);
