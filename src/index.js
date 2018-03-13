afa64e292affdd2642651c8428020137cfae00d1
njang <neo.w.jang@gmail.com>
2018-03-13 00:10:51 -0500

    Revise console message

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import Main from './Main';
 import registerServiceWorker from './registerServiceWorker';
 
-console.log('Hi there, fellow developer! Thanks for checking under the hood.')
+console.log('Hi there, fellow developer! Thanks for checking under the hood. Please feel free to reach out and say hello!')
 
 ReactDOM.render(<Main />, document.getElementById('root'));
 registerServiceWorker();
