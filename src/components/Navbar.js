87109ff57866cbb891e5b7d72767f3581034091b
njang <neo.w.jang@gmail.com>
2018-03-12 22:19:45 -0500

    Refactor redundancy in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 class Navbar extends Component {
 	render() {
 		return (
-			<nav className='navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top'>
+			<nav className='navbar fixed-top navbar-expand-lg navbar-dark bg-dark'>
 				<div className='container'>
 					<NavLink className='navbar-brand' to='/'>Neo Jang</NavLink>
 					<button className='navbar-toggler navbar-toggler-right' type='button' data-toggle='collapse' data-target='#navbarResponsive' aria-controls='navbarResponsive' aria-expanded='false' aria-label='Toggle navigation'>
